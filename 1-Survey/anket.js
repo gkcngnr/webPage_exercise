f521e8b80b4b229a30dd569c759650b94b76c6f8
N.G.Guner <gkcngnr@gmail.com>
2023-04-09 23:34:48 +0300

    survey exc first commit

---
+++ 1-Survey/anket.js
@@ -0,0 +1,65 @@
+
+const form = document.querySelector(".question-form");
+const result = document.querySelector(".result");
+
+
+form.addEventListener("submit", e=> {
+    e.preventDefault(); 
+    const user = []
+    const userAnsw = document.querySelectorAll(".form-check-input:checked")
+    userAnsw.forEach((answer) => {
+        user.push(answer.value)
+    })
+    
+    let totalQuestion = document.querySelectorAll(".form-check-input").length / 2; 
+    
+    
+    yes = 0;
+    user.forEach((answer) => {
+        if (answer === "e") {
+            yes++
+        }
+    })
+    score = Math.floor(yes / totalQuestion * 100)
+    
+    result.classList.remove("d-none");
+
+    let puan = 0;
+    const bastir = setInterval(() => {
+        result.querySelector("span").textContent = `%${puan}`;
+        if (puan === score) {
+            clearInterval(bastir);
+        } else {
+            puan++
+        }
+    }, 5)
+    
+    result.querySelector("#bar").style.width = (score + "%")
+
+
+
+})
+
+    
+
+
+
+
+// setTimeout(() => {
+//     console.log("asd")
+// }, 2000)
+
+// setInterval(() => {            // verilen aralıklarla çalışr
+//     console.log("asd")
+// }, 2000)
+
+// let i = 0;
+// const bastir = setInterval(() => {            
+//     console.log("asd")
+//     i++;
+//     if (i === 4) {
+//         clearInterval(bastir)           // verilen değerde durur
+//     }
+// }, 2000)
+
+
